d94b7feb9cb8b52b19ef2c1ab049e7928bb6579f
dataworkchile <webmaster@datawork.cl>
2023-11-19 00:51:33 -0300

    js_metodos

--- assets/js/js_funciones.js
+++ assets/js/js_funciones.js
@@ -6,6 +6,8 @@ let id_Tareas_tabla     = document.getElementById('id_Tareas_tabla')
 
 
 let array_contenidoTareas = []
+let array_salidarArregloRepetido = [];
+
 let htmlTareas = ''
 let countRealizadas = 0;
 let countTotalTareas = 0;
@@ -26,24 +28,29 @@ window.addEventListener("load", function () {
 
 });
 
-
+let array_tareasTerminadas = []
 // funcion para tareas terminadas
-function f_tarea_terminada(){
+function f_tarea_terminada(op){
     
     // recorro primero todos los imput y luego selecciono solo los checkbox, luego cuento solo los checkbox que son true
     let checkMarcados = document.getElementsByTagName('input').length
 
     countRealizadas = 0;
+   
 
     for (let ck=0; ck < checkMarcados; ck++){
         let checkbox = document.getElementsByTagName("input")[ck].checked
         
-        if (checkbox)
+        if (checkbox){
+            array_tareasTerminadas.push(op);
             countRealizadas++
-        
+        }
     }
-
+            
     id_TareasRealizadas.innerHTML = countRealizadas
+
+    
+
 }
 
 
@@ -77,16 +84,27 @@ btn_agregar.addEventListener('click', function() {
 
 function f_llenar_datos(){
 
+
     countTotalTareas = array_contenidoTareas.length;
+
+    let tareas_terminadas = f_ArrayRepetidos(array_tareasTerminadas);
 
     htmlTareas = '';
     for (let tarea of array_contenidoTareas){
+
+        // aqui extraigo los valores sin repetir de las tareas terminadas
+        let tarea_terminada = tareas_terminadas.includes(tarea.id)
+
+        // si la tarea terminada esta en el arreglo vendra el valor verdadero, por lo cual se carcara como realizada
+        let marcarCheck = '';
+        if (tarea_terminada)
+            marcarCheck = 'checked'
 
         htmlTareas+= `
                     <tr>
                     <td>${tarea.id}</td>
                     <td>${tarea.nombre}</td>
-                    <td><input class="form-check-input" type="checkbox" id="id_check" onclick="f_tarea_terminada()"></td>
+                    <td><input class="form-check-input" type="checkbox" id="id_check"  onclick="f_tarea_terminada(${tarea.id})" value"${tarea.id}" ${marcarCheck}></td>
                     <td><button type="button" class="btn btn-danger btn-sm"  onclick="f_tarea_eliminada(${tarea.id})">Eliminar</button></td>
                     </tr>
                     `
@@ -95,4 +113,13 @@ function f_llenar_datos(){
         id_Tareas_tabla.innerHTML = htmlTareas;
         id_TotalTareas.innerHTML = countTotalTareas
 
+}
+
+
+
+
+// aqui quito los valores repetidos de un arreglo; esto se hace para buscar los que estaban terminados despues de eliminar 
+function f_ArrayRepetidos(arreglo) {
+    let array_salidarArregloRepetido = Array.from(new Set(arreglo))
+    return array_salidarArregloRepetido
 }
